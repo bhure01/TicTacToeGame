8d3feb776e42b8a2c7ed489dd161cf8147cd53b6
bhure01 <dattatribhure@gmail.com>
2022-01-09 21:21:28 +0530

    Modified: allowing the player to choose X or O

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,18 +1,37 @@
 package com.bridgelabz;
-
-import java.sql.Array;
+import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game");
-        createBoard();
+
+    private static char[] boardArray = new char[10];
+    private static char player1Choice;
+    private static char player2Choice;
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void initiateBoard() {                                // board initialization -uc1
+        for (int i = 1; i < 10; i++) {
+            boardArray[i] = ' ';
+        }
     }
 
-    public static void createBoard() {
-        char[] arr = new char[10];
-
-        for(int i=1; i<=10; i++) {
-            arr[i] = ' ';
+    public static void playerChoice() {                                  //getting user inputs - uc2
+        System.out.println("Enter Player 1 choice (X / O) :");
+        char choice = scan.next().toUpperCase().charAt(0);
+        if (choice == 'X') {
+            player1Choice = choice;
+            player2Choice = 'O';
+        } else if (choice == 'O') {
+            player1Choice = choice;
+            player2Choice = 'X';
+        } else {
+            System.out.println("Wrong Choice Entered.");
+            playerChoice();
         }
+    }
+
+    public static void main(String[] args) {
+        initiateBoard();
+        playerChoice();
     }
 }
